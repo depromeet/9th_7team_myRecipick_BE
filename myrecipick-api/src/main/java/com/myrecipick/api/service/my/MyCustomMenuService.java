d8160fd1695d1794db291e7f1e75b9fcb9bf8a4d
이기승 <keeseung@sixshop.com>
2021-05-29 16:35:55 +0900

    feat: 커스텀 메뉴 여러개 삭제 기능 추가

--- myrecipick-api/src/main/java/com/myrecipick/api/service/my/MyCustomMenuService.java
+++ myrecipick-api/src/main/java/com/myrecipick/api/service/my/MyCustomMenuService.java
@@ -2,6 +2,7 @@ package com.myrecipick.api.service.my;
 
 import com.myrecipick.core.domain.my.MyCustomMenu;
 import com.myrecipick.core.domain.my.MyCustomMenuRepository;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -30,5 +31,9 @@ public class MyCustomMenuService {
 
     public Mono<UUID> delete(UUID customMenuId) {
         return myCustomMenuRepository.delete(customMenuId);
+    }
+
+    public Mono<Boolean> deletes(List<UUID> customMenuId) {
+        return myCustomMenuRepository.deletes(customMenuId);
     }
 }
